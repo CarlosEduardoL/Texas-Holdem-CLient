48cc7fad1b2b7b7cb41404877d5152a1e5a96a77
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2019-03-12 13:09:04 -0500

    Cuarto Commit

--- src/controller/MainScreenController.java
+++ src/controller/MainScreenController.java
@@ -3,9 +3,13 @@ package controller;
 import communication.TCPConnection;
 import view.MainScreen;
 
-public class MainScreenController implements TCPConnection.ConnectionEvent {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainScreenController implements TCPConnection.ConnectionEvent, ActionListener {
     
     private MainScreen view;
+    private boolean sali = false;
     
     public MainScreenController(){
         init();
@@ -14,6 +18,8 @@ public class MainScreenController implements TCPConnection.ConnectionEvent {
     private void init() {
     }
 
+
+
     @Override
     public void onConnection(String ip) {
 
@@ -21,6 +27,15 @@ public class MainScreenController implements TCPConnection.ConnectionEvent {
 
     @Override
     public void onMessage(String msj) {
+        if (!sali){
 
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getActionCommand().equals("Salirse")){
+            sali = true;
+        }
     }
 }
--- src/view/MainScreen.java
+++ src/view/MainScreen.java
@@ -1,10 +1,14 @@
 package view;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class MainScreen extends JFrame{
 
     public MainScreen(){
+
+        setSize(600,600);
+        setLayout(new BorderLayout());
 
     }
 
